69de620695c6ee84c78b70f1655e66ecadbdc569
Oskey999 <osacr.rafiq@gmail.com>
2023-04-20 15:14:51 +0800

    all

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
-#from flask import Flask, render_template
+from flask import Flask, render_template
 #https://pythonprogramming.net/sockets-tutorial-python-3/
-import socket 
-
-# create the socket
-# AF_INET == ipv4
-# SOCK_STREAM == TCP
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1234))
-s.listen(2)
-while True:
-    # now our endpoint knows about the OTHER endpoint.
-    clientsocket, address = s.accept()
-    print(f"Connection from {address} has been established.")
-    clientsocket.send(bytes("Hey there!!!","utf-8"))
+import socket
 
 
 
-# app = Flask(__name__)
-# @app.route('/')
+
+
+app = Flask(__name__)
+@app.route('/')
 # def home():
 #     return render_template("index.html")
 
-# def hello_world():
-#     return 'Hello world!'
+def hello_world():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((socket.gethostname(), 1234))
+
+    msg = s.recv(1024)
+    msg.decode("utf-8")
+    return msg
